c1a77eb75ad56af89c5c656be066d67d01656ca7
r1TOASTER <uritsamir@gmail.com>
2025-08-12 18:37:30 +0300

    fixed handlers / isrs calls

---
+++ Singularity-OS/arch/aarch64/isr.c
@@ -0,0 +1,16 @@
+// TODO: make sure to switch about the exception type - Sync, IRQ, FIQ, SError
+void el1_sp0_aarch64_isr() {
+
+}
+
+void el1_sp1_aarch64_isr() {
+    
+}
+
+void el0_sp0_aarch64_isr() {
+    
+}
+
+void el0_sp0_aarch32_isr() {
+    
+}
